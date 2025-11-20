fc3f57644ba31d5cd5efdb6d8aaf28860f3e69d8
mdabuzafar <mdabuzafar78@gmail.com>
2025-11-20 12:30:37 +0530

    refactor: move step navigation logic to a new Steps component

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,9 @@
-import { useState } from 'react';
-
-const messages = [
-  'Learn React ⚛️',
-  'Apply for jobs 💼',
-  'Invest your new income 🤑',
-];
+import Steps from './components/Steps';
 
 export default function App() {
-  const [step, setStep] = useState(1);
-
-  const handleNextClick = () => {
-    if (step >= 3) {
-      return;
-    }
-
-    setStep((s) => s + 1);
-  };
-
-  const handlePreviousClick = () => {
-    if (step <= 1) {
-      return;
-    }
-
-    setStep((s) => s - 1);
-  };
-
   return (
-    <div className='steps'>
-      <div className='numbers'>
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
-      </div>
-
-      <p className='message'>
-        Step {step}: {messages[step - 1]}
-      </p>
-
-      <div className='buttons'>
-        <button
-          style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handlePreviousClick}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handleNextClick}
-        >
-          Next
-        </button>
-      </div>
+    <div>
+      <Steps />
     </div>
   );
 }
